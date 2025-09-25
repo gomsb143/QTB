e3766348e73a7505b0ab7d25ffb80b4a23e0ac94
goms <gomsbalan@gmail.com>
2025-09-25 07:09:48 +0530

    Update Task card.tsx

--- src/components/Task card.tsx
+++ src/components/Task card.tsx
@@ -1,1 +1,9 @@
+import React from 'react';
 
+export default function TaskCard({ title }: { title: string }) {
+  return (
+    <div className="bg-white p-2 rounded shadow cursor-pointer hover:bg-gray-50">
+      {title}
+    </div>
+  );
+}
